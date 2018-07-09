a29de3decbdc64e3828b28d5e70cc45694477218
Andrii Natalenko <fineftp@gmail.com>
2018-07-09 12:29:46 +0300

    minimize example

--- src/try.py
+++ src/try.py
@@ -14,42 +14,10 @@ b = param[1] # 1-6
 c = param[2] # 1-3
 
 if a == "rec":
-    try:
-        print ("/tmp/"+b+"/"+c+".wav")
-    except subprocess.TimeoutExpired:
-        print ("Fail")
+    print ("/tmp/"+b+"/"+c+".wav")
 
 elif a == "play":
     print ("/tmp/"+b+"/"+c+".wav")
 
 elif a == "compile":
-    if b == "1":
-        try:
-            print ("/resources/training_service.sh /tmp/1/1.wav /tmp/1/2.wav /tmp/1/3.wav convert to /resources/model"+b+".pmdl")
-        except:
-            print ("Fail")
-    elif b == "2":
-        try:
-            print ("/resources/training_service.sh /tmp/2/1.wav /tmp/2/2.wav /tmp/2/3.wav convert to /resources/model"+b+".pmdl")
-        except:
-            print ("Fail")
-    elif b == "3":
-        try:
-            print ("/resources/training_service.sh /tmp/3/1.wav /tmp/3/2.wav /tmp/3/3.wav convert to /resources/model"+b+".pmdl")
-        except:
-            print ("Fail")
-    elif b == "4":
-        try:
-            print ("/resources/training_service.sh /tmp/4/1.wav /tmp/4/2.wav /tmp/4/3.wav convert to /resources/model"+b+".pmdl")
-        except:
-            print ("Fail")
-    elif b == "5":
-        try:
-            print ("/resources/training_service.sh /tmp/5/1.wav /tmp/5/2.wav /tmp/5/3.wav convert to /resources/model"+b+".pmdl")
-        except:
-            print ("Fail")
-    elif b == "6":
-        try:
-            print ("/resources/training_service.sh /tmp/6/1.wav /tmp/6/2.wav /tmp/6/3.wav convert to /resources/model"+b+".pmdl")
-        except:
-            print ("Fail")
+    print ("/resources/training_service.sh /tmp/"+b+"/1.wav /tmp/"+b+"/2.wav /tmp/"+b+"/3.wav convert to /resources/model"+b+".pmdl")
