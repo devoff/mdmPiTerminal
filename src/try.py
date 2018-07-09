40166784bab943fd99478b4042070eeb0b5963a0
Andrii Natalenko <fineftp@gmail.com>
2018-07-09 12:20:43 +0300

    fixes in example

--- src/try.py
+++ src/try.py
@@ -9,9 +9,9 @@ if len(sys.argv) == 1:
 text = sys.argv[1]
 # Разбираем аргумент на куски
 param = text.split("_")
-a = param[0]
-b = param[1]
-c = param[2]
+a = param[0] # rec, play или compile
+b = param[1] # 1-6
+c = param[2] # 1-3
 
 if a == "rec":
     try:
@@ -23,33 +23,33 @@ elif a == "play":
     print ("/tmp/"+b+"/"+c+".wav")
 
 elif a == "compile":
-    if c == "1":
+    if b == "1":
         try:
-            print ("/resources/training_service.sh /tmp/1/1.wav /tmp/1/2.wav /tmp/1/3.wav convert to /resources/model"+c+".pmdl")
+            print ("/resources/training_service.sh /tmp/1/1.wav /tmp/1/2.wav /tmp/1/3.wav convert to /resources/model"+b+".pmdl")
         except:
             print ("Fail")
-    elif c == "2":
+    elif b == "2":
         try:
-            print ("/resources/training_service.sh /tmp/2/1.wav /tmp/2/2.wav /tmp/2/3.wav convert to /resources/model"+c+".pmdl")
+            print ("/resources/training_service.sh /tmp/2/1.wav /tmp/2/2.wav /tmp/2/3.wav convert to /resources/model"+b+".pmdl")
         except:
             print ("Fail")
-    elif c == "3":
+    elif b == "3":
         try:
-            print ("/resources/training_service.sh /tmp/3/1.wav /tmp/3/2.wav /tmp/3/3.wav convert to /resources/model"+c+".pmdl")
+            print ("/resources/training_service.sh /tmp/3/1.wav /tmp/3/2.wav /tmp/3/3.wav convert to /resources/model"+b+".pmdl")
         except:
             print ("Fail")
-    elif c == "4":
+    elif b == "4":
         try:
-            print ("/resources/training_service.sh /tmp/4/1.wav /tmp/4/2.wav /tmp/4/3.wav convert to /resources/model"+c+".pmdl")
+            print ("/resources/training_service.sh /tmp/4/1.wav /tmp/4/2.wav /tmp/4/3.wav convert to /resources/model"+b+".pmdl")
         except:
             print ("Fail")
-    elif c == "5":
+    elif b == "5":
         try:
-            print ("/resources/training_service.sh /tmp/5/1.wav /tmp/5/2.wav /tmp/5/3.wav convert to /resources/model"+c+".pmdl")
+            print ("/resources/training_service.sh /tmp/5/1.wav /tmp/5/2.wav /tmp/5/3.wav convert to /resources/model"+b+".pmdl")
         except:
             print ("Fail")
-    elif c == "6":
+    elif b == "6":
         try:
-            print ("/resources/training_service.sh /tmp/6/1.wav /tmp/6/2.wav /tmp/6/3.wav convert to /resources/model"+c+".pmdl")
+            print ("/resources/training_service.sh /tmp/6/1.wav /tmp/6/2.wav /tmp/6/3.wav convert to /resources/model"+b+".pmdl")
         except:
             print ("Fail")
