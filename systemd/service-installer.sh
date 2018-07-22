f77ce0765fb1756d04f29b03c03f7dd560a098a0
Unknown <fineftp@gmail.com>
2018-07-22 19:18:34 +0300

    fix Installer

--- systemd/service-installer.sh
+++ systemd/service-installer.sh
@@ -3,19 +3,20 @@
 sudo cp systemd/010_services /etc/sudoers.d/
 
 echo
-while read -p $'Please select device type you want to configure:\n 1 - USB microphone\n 2 - OPi built-in microphone\n 3 - PS3 eye\nDevice type selected: ' DEVICETYPE ; do
-  if [[ $DEVICETYPE == 1 ]]; then
-    sudo cp systemd/asound_usb.conf /etc/asound.conf
-  elif [[ `echo $DEVICETYPE` == 2 ]]; then
-    sudo cp systemd/asound_pi.conf /etc/asound.conf
-  elif [[ `echo $DEVICETYPE` == 3 ]]; then
-    sudo cp systemd/asound_eye.conf /etc/asound.conf
-  else
-    echo
-    echo 'Wrong device type selected.'
-    echo
-  fi
-done
+read -p $'Please select device type you want to configure:\n 1 - USB microphone\n 2 - OPi built-in microphone\n 3 - PS3 eye\nDevice type selected: ' DEVICETYPE ;
+if [[ $DEVICETYPE == 1 ]]; then
+  sudo cp systemd/asound_usb.conf /etc/asound.conf
+elif [[ `echo $DEVICETYPE` == 2 ]]; then
+  sudo cp systemd/asound_pi.conf /etc/asound.conf
+elif [[ `echo $DEVICETYPE` == 3 ]]; then
+  sudo cp systemd/asound_eye.conf /etc/asound.conf
+else
+  echo
+  echo 'Wrong device type selected.'
+  echo
+  exit 1
+fi
+
 
 cp /dev/null mdmpiterminal.service
 
