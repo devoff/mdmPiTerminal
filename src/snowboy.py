cb3c7f6497669be3e189c6b8350f241cce4c55e2
Andrii Natalenko <fineftp@gmail.com>
2018-07-23 12:07:11 +0300

    refactoring

--- src/snowboy.py
+++ src/snowboy.py
@@ -2,7 +2,6 @@ import pyaudio
 import snowboydecoder
 import sys
 import signal
-#import RPi.GPIO as GPIO
 import time
 import os
 import subprocess
@@ -14,34 +13,21 @@ import random
 import socket
 import fcntl
 import struct
-##### Настройки #####
-#Название файлов модели.
-#model1 = 'model1.pmdl'
-#model2 = 'model2.pmdl'
-#Путь к файлу конфигурации
+
 home = os.path.abspath(os.path.dirname(__file__))
 path = home+'/settings.ini'
 config = configparser.ConfigParser()
-#Приветствие
-#subprocess.Popen(["aplay", home+"/snd/Startup.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 interrupted = False
-#Ссылки на голосовые модели
-#models = [home+'/resources/'+model1, home+'/resources/'+model2]
 models = []
 root_dir = home+'/resources/models/'
 for files in os.walk(root_dir):
     for m in files[2]:
         models.append(home+'/resources/models/'+m)
-# Загрузка конфига
+
 def getConfig (path):
     try:
-        global ID, TITLE, NAME, LINKEDROOM, PROVIDERTTS, APIKEYTTS, PROVIDERSTT, APIKEYSTT, SENSITIVITY, ALARMKWACTIVATED, ALARMTTS, ALARMSTT, IP, IP_SERVER, FIRSTBOOT
+        global PROVIDERTTS, APIKEYTTS, PROVIDERSTT, APIKEYSTT, SENSITIVITY, ALARMKWACTIVATED, ALARMTTS, ALARMSTT, IP_SERVER
         config.read(path)
-        #ID = config.get("Settings", "ID") #номер терминала
-        #TITLE = config.get("Settings", "TITLE") #навазние терминала
-        #NAME = config.get("Settings", "NAME") #Системное имя
-        #LINKEDROOM = config.get("Settings", "LINKEDROOM") #Расположение
-        #IP = config.get("Settings", "IP")
         PROVIDERTTS = config.get("Settings", "PROVIDERTTS") # Сервис синтеза речи
         APIKEYTTS = config.get("Settings", "APIKEYTTS") #Ключ API сервиса синтеза речи:
         PROVIDERSTT = config.get("Settings", "PROVIDERSTT") #Сервис распознования речи
@@ -51,7 +37,6 @@ def getConfig (path):
         ALARMTTS = config.get("Settings", "ALARMTTS") #Сигнал перед сообщением
         ALARMSTT = config.get("Settings", "ALARMSTT") #Сигнал перед начале распознования речи
         IP_SERVER = config.get("Settings", "IP_SERVER") #Сервер МДМ
-        FIRSTBOOT = config.get("Boot", "firstboot")
         print ("Конфигурация загружена")
     except:
         say ("Не создан файл конфигурации или ошибка в файле, загрузите данные через модуль в МДМ")
@@ -68,12 +53,11 @@ def detected():
     try:
         getConfig (path)
         detector.terminate()
-        #snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
         if ALARMKWACTIVATED == "1":
             subprocess.Popen(["aplay", home+"/snd/ding.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             print (SENSITIVITY)
-        index = pyaudio.PyAudio().get_device_count() - 1
-        print (index)
+        #index = pyaudio.PyAudio().get_device_count() - 1
+        #print (index)
         with sr.Microphone() as source:
             r = sr.Recognizer()
             r.adjust_for_ambient_noise(source) # Слушаем шум 1 секунду, потом распознаем, если раздажает задержка можно закомментировать.
@@ -82,7 +66,6 @@ def detected():
             audio = r.listen(source, timeout = 10, phrase_time_limit=15)
             if ALARMTTS == "1":
                 subprocess.Popen(["aplay", home+"/snd/dong.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            #snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
             print("Processing ... Для распознования используем "+PROVIDERSTT)
             if PROVIDERSTT == "Google":
                 command=r.recognize_google(audio, language="ru-RU")
@@ -93,7 +76,6 @@ def detected():
             print(command)
             if ALARMTTS == "1":
                subprocess.Popen(["aplay", home+"/snd/dong.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            #snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
             link="http://"+IP_SERVER+'/command.php?qry=' + urllib.parse.quote_plus(command)
             f=urllib.request.urlopen(link)
     except  sr.UnknownValueError:
@@ -104,30 +86,20 @@ def detected():
     except sr.WaitTimeoutError:
             random_item = random.SystemRandom().choice(["Вы что то сказали ?", "Я ничего не услышала", "Что Вы спросили?", "Не поняла"])
             say (random_item)
-            #say ("говорите после сигнала")
     except request.ConnectionResetError:
             say ("Ошибка соединения с сервером распознования речи")
     detector.start(detected_callback=callbacks,
                interrupt_check=interrupt_callback,
                 sleep_time=0.03)
-#Узнаем IP адрес
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
-# Загрузка конфига
+
 getConfig (path)
-#capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
-#sensitivity = [SENSITIVITY1]*len(models) #уровень распознования, чем больше значение, тем больше ложных срабатываней
-sensitivity = [SENSITIVITY] #уровень распознования, чем больше значение, тем больше ложных срабатываней
+sensitivity = [SENSITIVITY]
 detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
-#callbacks = [detected, detected]
 callbacks = []
 for l in models:
     callbacks.append(detected)
-# main loop
-# make sure you have the same numbers of callbacks and models
+
 print('Слушаю... Нажмите Ctrl+C для выхода')
 detector.start(detected_callback=callbacks,
                interrupt_check=interrupt_callback,
